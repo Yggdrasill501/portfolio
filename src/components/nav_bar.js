9141c7321289ef7a73bff8ea6269d892dcb1bc0a
Filip Žitný <filipzitny@gmail.com>
2023-07-30 00:49:22 +0200

    navbar

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,22 +1,19 @@
-import { Container, Nav, NavDropdown } from "react-bootstrap"
+import { Container, Nav,} from "react-bootstrap"
 
 export const NavBar = () => {
     return ( 
-        <NavBar bg="light" expand="lg">
+        <NavBar expand="lg">
             <Container>
-                <NavBar.Brand href="#home">React-Bootstrap</NavBar.Brand>
-                <NavBar.Toggle aria-controls="basic-navbar-nav" />
+                <NavBar.Brand href="#home">
+                    <img src={''} alt="Logo"/>
+                </NavBar.Brand>
+                <NavBar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </NavBar.Toggle>
                 <NavBar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
                 </NavBar.Collapse>
             </Container>
