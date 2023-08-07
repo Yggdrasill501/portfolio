ec129364efc49ecc4a7c1c22c4abbbe152b8fe9b
Filip Žitný <filipzitny@gmail.com>
2023-08-07 16:31:42 +0200

    map

---
+++ src/components/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    // Create a new map instance
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 37.7749, lng: -122.4194 }, // Set the initial center of the map
+      zoom: 10 // Set the initial zoom level
+    });
+  
+    // You can also add markers, polygons, and other features to the map here
+  }
