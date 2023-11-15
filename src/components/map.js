999f1c3983560a4ac417fdc025aba082061cf077
Filip Žitný <filipzitny@gmail.com>
2023-11-15 01:36:54 +0100

    nav bar update logo scroll typos

--- src/components/map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    // Create a new map instance
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 37.7749, lng: -122.4194 }, // Set the initial center of the map
-      zoom: 10 // Set the initial zoom level
-    });
-  
-    // You can also add markers, polygons, and other features to the map here
-  }
--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,21 +1,60 @@
-import { Container, Nav,} from "react-bootstrap"
+import {useEffect, useState} from "react";
+import {Container, Nav, Navbar, NavbarBrand, NavbarToggle} from "react-bootstrap";
+import logo from "../assets/icons/logo.png";
+import navIcon1 from "../assets/icons/nav-icon1.png";
+import navIcon2 from "../assets/icons/nav-icon2.png";
+import navIcon3 from "../assets/icons/nav-icon3.png";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, seScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                seScrolled(true);
+            } else {
+                seScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (link) => {
+        setActiveLink(link);
+    }
+
     return ( 
-        <NavBar expand="lg">
+        <NavBar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
-                <NavBar.Brand href="#home">
-                    <img src={''} alt="Logo"/>
-                </NavBar.Brand>
-                <NavBar.Toggle aria-controls="basic-navbar-nav">
+                <NavbarBrand href="#home">
+                    <img src={logo} alt="Logo"/>
+                </NavbarBrand>
+                <NavbarToggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
-                </NavBar.Toggle>
-                <NavBar.Collapse id="basic-navbar-nav">
+                </NavbarToggle>
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                     </Nav>
-                </NavBar.Collapse>
+                    <span className="navbar-text">
+                        <div className="social-icons">
+                            <a href="#"><img src={navIcon1} alt="" /></a>
+                            <a href="#"><img src={navIcon2} alt="" /></a>
+                            <a href="#"><img src={navIcon3} alt="" /></a>
+                        </div>
+                    </span>
+                    <button className="btn-main-offer" onClick={() => console.log('connect')}>
+                        <span>
+                            Let's Connect
+                        </span>
+                    </button>
+                </Navbar.Collapse>
             </Container>
         </NavBar>
     )
